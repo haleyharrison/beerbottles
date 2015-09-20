79617fc65d516b81a76c31388655201791a3ab6d
haleyharrison <harrison.haley@gmail.com>
2015-09-20 15:26:51 -0700

    added function

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,22 @@
-var beerCount = function()
+  var beerCount = function(decrease, bottles) {
+    var integers = [];
+      for (var i = 1; i <= bottles; i++) {
+        if (i - decrease1 === 0) {
+          integers.push(i);
+        }
+      }
+return integers;
+}
+
+$(document).ready(function() {
+  $("form#count-by").submit(function(event) {
+    var decrease1 = parseInt($("input#decrease1").val());
+    var bottles = parseInt($("input#bottles").val());
+    var result = beerCount(decrease1, bottles);
+
+    $(".output").text(result);
+    $("result").show();
+    event.preventDefault();
+
+});
+});
