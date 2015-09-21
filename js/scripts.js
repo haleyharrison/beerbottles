8fca55d53452ac58b171305b09cdc6e7c77ba6ea
haleyharrison <harrison.haley@gmail.com>
2015-09-20 18:09:59 -0700

    functionality working

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,31 @@
   var beerCount = function(bottlesNumber) {
   //var integers = [];
-  //  for (var i = 1; i <= bottles; i-) {
+  //for (var i = 1; i <= bottles; i-) {
 
 
-    if (bottlesNumber === 0) {
-      return "no more bottles of beer!";
-    } else if (bottlesNumber === 1) {
-      "1 bottle of beer on the wall, 1 bottle of beer";
-    } else if (bottlesNumber === 2 {
-      "2 bottles of beer on the wall, 2 bottles of beer";
-      else {
-      return bottlesNumber + "bottles of beer on the wall," + bottlesNumber
-      "bottles of beer, take one down and pass it around," +
-      bottlesNumber - 1 + "bottles of beer on the wall!"
-      + beerCount(bottlesNumber - 1);
+     if (bottlesNumber === 0) {
+       return "no more bottles of beer!";
+     } else if (bottlesNumber === 1) {
+       return "1 bottle of beer on the wall, 1 bottle of beer!";
+     } else if (bottlesNumber === 2) {
+       return "2 bottles of beer on the wall, 2 bottles of beer!";
+     } else {
+       return bottlesNumber + " bottles of beer on the wall, " + bottlesNumber +
+       " bottles of beer, take one down and pass it around, " +
+       (bottlesNumber - 1) + " bottles of beer on the wall! "
+       + beerCount(bottlesNumber - 1);
+   }
+
       }
-    })
-    //integers.push(i);
-        }
-      }
-    return integers;
-}
+
 
 $(document).ready(function() {
   $("form#beerBottles").submit(function(event) {
-    //var decrease1 = parseInt($("input#decrease1").val());
-    var bottles = parseInt($("input#bottlesNumber").val());
+    var bottlesNumber = parseInt($("input#bottlesNumber").val());
     var result = beerCount(bottlesNumber);
 
     $(".output").text(result);
-    $("result").show();
+    $("#result").show();
     event.preventDefault();
 
 });
