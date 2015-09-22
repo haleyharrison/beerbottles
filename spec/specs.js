1a90631154e41d39dbd0039e3a5f8fd62e2d4983
haleyharrison <harrison.haley@gmail.com>
2015-09-22 09:54:31 -0700

    working tests

--- spec/specs.js
+++ spec/specs.js
@@ -1,6 +1,29 @@
 
+//when 0 is entered
   describe('beerCount', function(){
-    it("will count down from the user inputted number by 1", function() {
-      expect(beerCount(9)).to.equal(8);
+    it("will return a 'no bottles left' message", function() {
+      expect(beerCount(0)).to.equal("no more bottles of beer!");
     })
   });
+
+
+//when 1 is entered
+  describe('beerCount', function(){
+    it("will return '1 bottle of beer on the wall, 1 bottle of beer!'", function() {
+      expect(beerCount(1)).to.equal("1 bottle of beer on the wall, 1 bottle of beer!");
+      })
+    });
+
+//when anything higher is entered
+  describe('beerCount', function(){
+    it("will return bottles of beer on the wall", function() {
+      expect(beerCount(3)).to.include("3 bottles of beer on the wall");
+        })
+      });
+
+//"not include" test
+  describe('beerCount', function(){
+    it("will return bottles of beer on the wall", function() {
+      expect(beerCount(4)).to.not.include("5 bottles of beer on the wall");
+        })
+      });
